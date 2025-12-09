ceb371288986c6ab46f9bd1f4fb8469cb9c11a42
Robespierre Triviño <robtrivi@espol.edu.ec>
2025-12-09 16:09:38 -0500

    feat: eliminar pantalla inicial y ajustar rutas de navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./features/initial/initial-screen.component').then(m => m.InitialScreenComponent)
-  },
-  {
-    path: 'visualization',
     loadComponent: () => import('./features/visualization/visualization-sigma.component').then(m => m.VisualizationSigmaComponent)
   },
   {
